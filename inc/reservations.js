304d589bc26d5d3c6f015cb72f10d2613fa83038
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-09 15:37:02 -0300

    feat: adds pagination to reservation table

--- inc/reservations.js
+++ inc/reservations.js
@@ -1,4 +1,5 @@
 var conn = require('./db');
+var Pagination = require("./../inc/Pagination");
 
 module.exports = {
     render(req, res, error, success){
@@ -69,18 +70,42 @@ module.exports = {
         
     },
 
-    getReservations(){
+    getReservations(req){
 
-        return new Promise((resolve, reject) => {
-            conn.query(`
-                SELECT * FROM tb_reservations ORDER BY date DESC
-                `, (err, results)=>{
-                  if(err){
-                    reject(err);
-                  }
-                  resolve(results);
-                });
-        });
+      return new Promise((resolve, reject) => {
+
+        let page = req.query.page;
+        let dtstart = req.query.dtstart;
+        let dtend = req.query.dtend;
+
+        if(!page) page = 1;
+
+        let params = []
+
+        if(dtend && dtstart) params.push(dtstart, dtend);
+
+        let pag = new Pagination(
+                  `
+                  SELECT SQL_CALC_FOUND_ROWS * 
+                  FROM tb_reservations 
+                  ${(dtstart && dtend) ? "WHERE date BETWEEN ? AND ?" : ""}
+                  ORDER BY name LIMIT ?, ?
+                  `,
+                  params
+                
+                )
+                
+
+          pag.getPage(page).then(data=>{
+            resolve({
+              data,
+              links: pag.getNavigation(req.query)
+            })
+          });
+
+      })
+
+      
     },
 
     delete(id){
@@ -104,3 +129,5 @@ module.exports = {
         });
       }
 }
+
+
--- routes/admin.js
+++ routes/admin.js
@@ -9,6 +9,7 @@ var reservations = require("./../inc/reservations");
 var contacts = require("./../inc/contacts");
 var emails = require("./../inc/emails");
 
+
 moment.locale("pt-BR");
 
 router.use(function(req, res, next){
@@ -148,9 +149,12 @@ router.delete("/menus/:id", function(req, res, next){
 //reserva
 router.get('/reservations', function(req, res, next) {
 
-    reservations.getReservations().then(data=>{
+    let start = (req.query.start) ? req.query.start : moment().subtract(1, "year").format("YYYY-MMM-DD");
+    let end = (req.query.end) ? req.query.end : moment().format("YYYY-MMM-DD");
 
-        res.render("admin/reservations", admin.getParams(req, { date: {}, data, moment }))
+    reservations.getReservations(req).then(pag=>{
+
+        res.render("admin/reservations", admin.getParams(req, { date: {start, end}, data: pag.data, moment, links: pag.links }))
 
     })
   
